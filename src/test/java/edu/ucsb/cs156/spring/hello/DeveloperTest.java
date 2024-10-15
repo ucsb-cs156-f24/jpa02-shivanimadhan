70f3926b26aa237f532e25f6fc4d89c62ff01896
Shivani Madhan <madhan.shivani1104@gmail.com>
2024-10-14 22:20:11 -0700

    Fix tests and coverage issues

--- src/test/java/edu/ucsb/cs156/spring/hello/DeveloperTest.java
+++ src/test/java/edu/ucsb/cs156/spring/hello/DeveloperTest.java
@@ -29,5 +29,22 @@ public class DeveloperTest {
 
     // TODO: Add additional tests as needed to get to 100% jacoco line coverage, and
     // 100% mutation coverage (all mutants timed out or killed)
+    
+    @Test
+    public void getGithubId_returns_correct_githubId() 
+    {
+	assertEquals("shivanimadhan", Developer.getGithubId());
+    }
 
+    @Test
+    public void getTeam_returns_team_with_correct_name() {
+        Team  t = Developer.getTeam();
+        assertEquals("f24-13", t.getName());
+	assertTrue(t.getMembers().contains("Shivani M."), "Team should contain Shivani M.");
+	assertTrue(t.getMembers().contains("Emily Z."), "Team should contain Emily Z.");
+	assertTrue(t.getMembers().contains("Anvitha K."), "Team should contain Anvitha K.");
+	assertTrue(t.getMembers().contains("Steven J."), "Team should contain Steven J.");
+	assertTrue(t.getMembers().contains("Andy S."), "Team should contain Andy S.");
+	assertTrue(t.getMembers().contains("David Q."), "Team should contain David Q.");
+    }
 }
--- src/test/java/edu/ucsb/cs156/spring/hello/TeamTest.java
+++ src/test/java/edu/ucsb/cs156/spring/hello/TeamTest.java
@@ -1,6 +1,7 @@
 package edu.ucsb.cs156.spring.hello;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -22,5 +23,89 @@ public class TeamTest {
    
     // TODO: Add additional tests as needed to get to 100% jacoco line coverage, and
     // 100% mutation coverage (all mutants timed out or killed)
+    @Test
+    public void toString_returns_correct_string() {
+        assertEquals("Team(name=test-team, members=[])", team.toString());
+    }
+
+    @Test
+    public void equals_same_object()
+    {
+        Team t1 = new Team();
+        assertTrue(t1.equals(t1));
+    }
+
+    @Test
+    public void different_classes()
+    {
+        Team t1 = new Team();
+        String str = "Not a team";
+        assertTrue(!t1.equals(str));
+    }
+
+    @Test
+    public void both_name_and_members_match()
+    {
+        Team t1 = new Team();
+        t1.setName("Team A");
+        t1.addMember("Alice");
+
+        Team t2 = new Team();
+        t2.setName("Team A");
+        t2.addMember("Alice");
+
+        assertTrue(t1.equals(t2));
+    }
+
+    @Test
+    public void only_names_match()
+    {
+        Team t1 = new Team();
+        t1.setName("Team A");
+        t1.addMember("Alice");
+
+        Team t2 = new Team();
+        t2.setName("Team A");
+        t2.addMember("Bob");
+
+        assertTrue(!t1.equals(t2));
+    }
+
+    @Test
+    public void only_members_match()
+    {
+        Team t1 = new Team();
+        t1.setName("Team A");
+        t1.addMember("Alice");
+
+        Team t2 = new Team();
+        t2.setName("Team B");
+        t2.addMember("Alice");
+
+        assertTrue(!t1.equals(t2));
+    }
+
+    @Test
+    public void neither_match()
+    {
+        Team t1 = new Team();
+        t1.setName("Team A");
+        t1.addMember("Alice");
+
+        Team t2 = new Team();
+        t2.setName("Team B");
+        t2.addMember("Bob");
+
+        assertTrue(!t1.equals(t2));
+    }
+
+    @Test
+    public void hash_code_check()
+    {
+        Team t = new Team();
+        int result = t.hashCode();
+        int expectedResult = 1;
+        assertEquals(expectedResult, result);
+    }
 
 }
